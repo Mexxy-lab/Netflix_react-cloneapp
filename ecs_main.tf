f58a8bfb524635b5f65d2d3e8fbe710271deec89
Emeka Umejiofor <pumej1985@gmail.com>
2023-11-12 15:19:05 -0500

    updated ecs main.tf file

--- ecs_main.tf
+++ ecs_main.tf
@@ -35,7 +35,7 @@ EOF
 # Create a service to run the task on the cluster
 resource "aws_ecs_service" "my_service" {
   name            = "pumej-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
+  cluster         = aws_ecs_cluster.pumej_cluster.id
   task_definition = aws_ecs_task_definition.my_task_definition.arn
   desired_count   = 1
   launch_type     = "FARGATE"
